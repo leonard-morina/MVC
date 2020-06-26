8aa23e921a7ffc8b8fea2965b172fc9131babf22
leonard-morina <leonardmorina1@gmail.com>
2020-06-26 15:39:09 +0200

    Add project files.

---
+++ AnnexAdmin/Data/ApplicationDbContext.cs
@@ -0,0 +1,54 @@
+﻿using AnnexAdmin.Utils;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnnexAdmin.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                var newTbl = entity.GetTableName().ToSnakeCase().Remove(entity.GetTableName().ToSnakeCase().Length - 1);
+                entity.SetTableName(newTbl);
+  
+                foreach (var property in entity.GetProperties())
+                {
+                    property.SetColumnName(property.GetColumnName().ToSnakeCase());
+                }
+
+                foreach (var key in entity.GetKeys())
+                {
+                    key.SetName(key.GetName().ToSnakeCase());
+                }
+
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    key.SetConstraintName(key.GetConstraintName().ToSnakeCase());
+                }
+
+                foreach (var index in entity.GetIndexes())
+                {
+                    index.SetName(index.GetName().ToSnakeCase());
+                }
+            }
+        }
+    }
+}
---
+++ AnnexAdmin/Migrations/20200626124853_Initial.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace AnnexAdmin.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "role",
+                columns: table => new
+                {
+                    id = table.Column<string>(nullable: false),
+                    name = table.Column<string>(maxLength: 256, nullable: true),
+                    normalized_name = table.Column<string>(maxLength: 256, nullable: true),
+                    concurrency_stamp = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_role", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<string>(nullable: false),
+                    user_name = table.Column<string>(maxLength: 256, nullable: true),
+                    normalized_user_name = table.Column<string>(maxLength: 256, nullable: true),
+                    email = table.Column<string>(maxLength: 256, nullable: true),
+                    normalized_email = table.Column<string>(maxLength: 256, nullable: true),
+                    email_confirmed = table.Column<bool>(nullable: false),
+                    password_hash = table.Column<string>(nullable: true),
+                    security_stamp = table.Column<string>(nullable: true),
+                    concurrency_stamp = table.Column<string>(nullable: true),
+                    phone_number = table.Column<string>(nullable: true),
+                    phone_number_confirmed = table.Column<bool>(nullable: false),
+                    two_factor_enabled = table.Column<bool>(nullable: false),
+                    lockout_end = table.Column<DateTimeOffset>(nullable: true),
+                    lockout_enabled = table.Column<bool>(nullable: false),
+                    access_failed_count = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "role_claim",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    role_id = table.Column<string>(nullable: false),
+                    claim_type = table.Column<string>(nullable: true),
+                    claim_value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_role_claim", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_role_claim_role_role_id",
+                        column: x => x.role_id,
+                        principalTable: "role",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_claim",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<string>(nullable: false),
+                    claim_type = table.Column<string>(nullable: true),
+                    claim_value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_claim", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_claim_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_login",
+                columns: table => new
+                {
+                    login_provider = table.Column<string>(maxLength: 128, nullable: false),
+                    provider_key = table.Column<string>(maxLength: 128, nullable: false),
+                    provider_display_name = table.Column<string>(nullable: true),
+                    user_id = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_login", x => new { x.login_provider, x.provider_key });
+                    table.ForeignKey(
+                        name: "fk_user_login_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_role",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(nullable: false),
+                    role_id = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_role", x => new { x.user_id, x.role_id });
+                    table.ForeignKey(
+                        name: "fk_user_role_role_role_id",
+                        column: x => x.role_id,
+                        principalTable: "role",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_user_role_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_token",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(nullable: false),
+                    login_provider = table.Column<string>(maxLength: 128, nullable: false),
+                    name = table.Column<string>(maxLength: 128, nullable: false),
+                    value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_token", x => new { x.user_id, x.login_provider, x.name });
+                    table.ForeignKey(
+                        name: "fk_user_token_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "role_name_index",
+                table: "role",
+                column: "normalized_name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_role_claim_role_id",
+                table: "role_claim",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "email_index",
+                table: "user",
+                column: "normalized_email");
+
+            migrationBuilder.CreateIndex(
+                name: "user_name_index",
+                table: "user",
+                column: "normalized_user_name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_claim_user_id",
+                table: "user_claim",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_login_user_id",
+                table: "user_login",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_role_role_id",
+                table: "user_role",
+                column: "role_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "role_claim");
+
+            migrationBuilder.DropTable(
+                name: "user_claim");
+
+            migrationBuilder.DropTable(
+                name: "user_login");
+
+            migrationBuilder.DropTable(
+                name: "user_role");
+
+            migrationBuilder.DropTable(
+                name: "user_token");
+
+            migrationBuilder.DropTable(
+                name: "role");
+
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
